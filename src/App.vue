c351e8e2b746562e06f8eb8652ef0d423fa4c181
recall <caironghang@do1.com.cn>
2019-05-04 22:51:47 +0800

    修改字体

--- src/App.vue
+++ src/App.vue
@@ -65,7 +65,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: ""微软雅黑",Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei",Arial,sans-serif;; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
